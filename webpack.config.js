d2720dd6feba0f01a303ec93fb310f7cbdd4806f
Damien Pijaczynski <dpijaczynski@tungstensoftware.fr>
2024-03-22 12:18:35 +0100

    Include css

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["css-loader"],
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(js|jsx|tsx|ts)$/,
